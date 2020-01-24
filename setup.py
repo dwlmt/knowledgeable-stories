8e71f8ac8f85b5d011e7e2d32304692584346b78
David Wilmot <david.wilmot@ed.ac.uk>
2020-01-24 21:27:12 +0000

    Initial version of the setup files.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# Use requirements text to manage the dependencies.
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='Knowledgeable-Stories',
+    version='0.0.1',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"],include=["data"]),
+    install_requires=requirements,
+    url='',
+    license='',
+    author='David Wilmot',
+    author_email='david.wilmot@ed.ac.uk',
+    description='2nd iteration of the story models to incorporate multitask learning and richer semantic knowledgebase, event and character based understanding.',
+)
