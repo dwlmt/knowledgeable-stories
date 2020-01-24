92adf04bead126e11e9f122c9701626d24f36270
David Wilmot <david.wilmot@ed.ac.uk>
2020-01-24 21:27:56 +0000

    First rough version of the atomic commonsense dataset reader and test using the pretrained tokenizer and indexer.

--- knowledgeablestories/readers/atomic_reader.py
+++ knowledgeablestories/readers/atomic_reader.py
@@ -1,12 +1,15 @@
+import ast
 import csv
 import logging
 from typing import Dict, List, Iterator
 
-from allennlp.data import DatasetReader, TokenIndexer, Instance, Token
-from allennlp.data.fields import TextField
-from allennlp.data.token_indexers import SingleIdTokenIndexer
+from allennlp.data import DatasetReader, TokenIndexer, Instance, Token, Tokenizer
+from allennlp.data.fields import TextField, MetadataField
+from allennlp.data.token_indexers import SingleIdTokenIndexer, PretrainedTransformerIndexer
 
 # Categories for relations in the commonsense reasoning dataset.
+from allennlp.data.tokenizers import PretrainedTransformerTokenizer
+
 atomic_categories = []
 atomic_categories += ["oEffect"]
 atomic_categories += ["oReact"]
@@ -24,27 +27,56 @@ class AtomicDatasetReader(DatasetReader):
     DatasetReader for Atomic reader dataset from https://github.com/atcbosselut/comet-commonsense
 
     """
-    def __init__(self, token_indexers: Dict[str, TokenIndexer] = None, categories=None) -> None:
+    def __init__(self, tokenizer: Tokenizer = None, token_indexers: Dict[str, TokenIndexer] = None, categories=None,
+                 start_and_end_tokens = False) -> None:
         super().__init__(lazy=False)
-        self.token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
 
-        if categories is None:
-            self.categories = atomic_categories
+        self.tokenizer = tokenizer or PretrainedTransformerTokenizer(model_name="gpt2", do_lowercase=False)
+        self.token_indexers = token_indexers or {"tokens": PretrainedTransformerIndexer(model_name="gpt2", do_lowercase=False)}
+        self.categories = categories or atomic_categories
 
-    def text_to_instance(self, tokens: List[Token], tags: List[str] = None) -> Instance:
-        sentence_field = TextField(tokens, self.token_indexers)
-        fields = {"sentence": sentence_field}
+        self.start_and_end_tokens = start_and_end_tokens
+
+    def text_to_instance(self, text_dict) -> Instance:
+        fields = {}
+
+        fields["metadata"] = MetadataField(text_dict)
+        for field in ["subject","object","relation"]:
+            tokens = self.tokenizer.tokenize(text_dict[field])
+            text_field = TextField(tokens, self.token_indexers)
+            fields[field] =  text_field
+
 
         return Instance(fields)
 
-    def _read(self, file_path: str) -> Iterator[Instance]:
-        with open(file_path) as f:
 
-            with open(file_path, mode='r') as csv_file:
-                csv_reader = csv.DictReader(csv_file)
-                line_count = 0
-                for row in csv_reader:
-                    print(row)
-                    line_count += 1
-                    yield row
-                logging.info(f'Atomic dataset has  {line_count} records.')
+    def _read(self, file_path: str) -> Iterator[Instance]:
+
+        with open(file_path, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            row_num = 0
+            for row in csv_reader:
+
+                for cat in self.categories:
+                    cat_data = row[cat]
+
+                    cat_data_list = ast.literal_eval(cat_data)
+                    cat_data_list = [n.strip() for n in cat_data_list]
+
+                    if len(cat_data_list) > 0:
+
+                        for cat_data_item in cat_data_list:
+
+                            relation_dict = {}
+
+                            relation_dict["dataset"] = "atomic"
+                            relation_dict["subject"] = row["event"].replace('___','<blank>')
+                            relation_dict["relation"] = cat
+                            relation_dict["object"] = cat_data_item.replace('_','<blank>')
+                            relation_dict["row_num"] = row_num
+
+                            row_num += 1
+
+                            yield self.text_to_instance(relation_dict)
+
+            logging.info(f'Atomic dataset {file_path} has  {row_num} examples.')
--- knowledgeablestories/tests/readers/atomic_reader_test.py
+++ knowledgeablestories/tests/readers/atomic_reader_test.py
@@ -1,22 +1,30 @@
+import os
+import pathlib
 from typing import cast
 
 import pytest
 
 from allennlp.common.util import ensure_list
-from allennlp.common.testing import AllenNlpTestCase
+from allennlp.common.testing import AllenNlpTestCase, test_case
 from allennlp.data.fields import TextField
 from allennlp.data.instance import Instance
 
 from knowledgeablestories.readers.atomic_reader import AtomicDatasetReader
 
-class TestAtomicDatasetReader:
-    def test_read(self, lazy):
+AllenNlpTestCase.MODULE_ROOT = (pathlib.Path(__file__).parent / ".." / ".." / "..").resolve()
+
+class TestAtomicDatasetReader(AllenNlpTestCase):
+    def test_read(self):
         reader = AtomicDatasetReader()
         instances = reader.read(
-            str(AllenNlpTestCase.FIXTURES_ROOT / "data" / "atomic_small.csv")
+            str(AllenNlpTestCase.MODULE_ROOT) + "/data/atomic_small.csv"
         )
         instances = ensure_list(instances)
 
         print(instances)
 
-        assert len(instances) == 100
+        assert len(instances) == 343 # Each relation is expanded out.
+
+        for instance in instances:
+            print(instance)
+            print(instance["metadata"])
