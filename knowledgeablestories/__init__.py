65a1552a3e1f569e6d67efecf8d9e9f4c2487293
David Wilmot <david.wilmot@ed.ac.uk>
2020-02-25 12:42:46 +0000

    Top level package init.

--- knowledgeablestories/__init__.py
+++ knowledgeablestories/__init__.py
@@ -1,1 +1,7 @@
-import knowledgeablestories
+from knowledgeablestories.dataset_readers import *
+from knowledgeablestories.models import *
+from knowledgeablestories.predictors import *
+from knowledgeablestories.modules import *
+from knowledgeablestories.iterators import *
+from knowledgeablestories.common import *
+from knowledgeablestories.trainers import *
