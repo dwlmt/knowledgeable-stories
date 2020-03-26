872cfe25c2e1e5373094374f9d9a6772751cf443
David Wilmot <david.wilmot@gmail.com>
2020-03-26 22:20:50 +0000

    Fix formatting.

--- scripts/evaluate_stories.py
+++ scripts/evaluate_stories.py
@@ -814,7 +814,7 @@ def create_peak_text_and_metadata(peak_indices, peaks_meta):
         left_base = peaks_meta["left_bases"][i]
         right_base = peaks_meta["right_bases"][i]
 
-        text = f"<br>Prominence: {prominence} <br>Width: {width}, <br> Left Base: {left_base}, <br> Right Base: {right_base}"
+        text = f"<br>Prominence: {prominence} <br>Width: {width}, <br>Left Base: {left_base}, <br>Right Base: {right_base}"
 
         hover_text.append(text)
     return hover_text
