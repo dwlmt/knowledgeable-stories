ab257b2483a1e9455cd5456a75f84799f44c8776
David Wilmot <david.wilmot@gmail.com>
2020-03-27 19:16:26 +0000

    Change the passage encoder to run one sentence at a time.

--- knowledgeablestories/predictors/knowledgeable_predictor.py
+++ knowledgeablestories/predictors/knowledgeable_predictor.py
@@ -164,7 +164,7 @@ class KnowledgeablePredictor(Predictor):
                         self.tree_generation([parent], [input_tokens], [merged_sentences_encoded],
                                              self._num_levels_rollout, original_sentences, story_idx)
 
-                        #print("Parent", parent)
+                        # print("Parent", parent)
                         self._calculate_metrics(parent, previous_prediction_metrics)
 
                         if not self._retain_full_output:
@@ -299,8 +299,6 @@ class KnowledgeablePredictor(Predictor):
 
                 logits /= self._prediction_temp
                 print(f"Logits {logits}, {logits.size()}")
-
-                logits = logits[0]
 
                 probs, log_probs = self._logits_to_probs(logits)
 
@@ -529,45 +527,33 @@ class KnowledgeablePredictor(Predictor):
         encoded_passages_list = []
         for encoded_sentences_batch_tensor in encoded_sentences_tensor:
 
-            #print(f"Join context, {merged_sentences_encoded.size()}, {encoded_sentences_tensor.size()}, {encoded_sentences_batch_tensor.size()}")
+            # print(f"Join context, {merged_sentences_encoded.size()}, {encoded_sentences_tensor.size()}, {encoded_sentences_batch_tensor.size()}")
 
             encoded_sentences_batch_tensor_expanded = torch.unsqueeze(encoded_sentences_batch_tensor, dim=0)
 
             print(f"Encoded expanded {encoded_sentences_batch_tensor_expanded}")
 
-            merged_sentences_encoded_expanded = torch.unsqueeze(merged_sentences_encoded, dim=1).expand(
-                merged_sentences_encoded.size(0),
-                encoded_sentences_batch_tensor_expanded.size(1),
-                merged_sentences_encoded.size(1))
 
-            context_sentences_to_encode = torch.cat(
-                (merged_sentences_encoded_expanded, encoded_sentences_batch_tensor_expanded))
+            context_sentences_to_encode = torch.unsqueeze(torch.cat(merged_sentences_encoded, encoded_sentences_batch_tensor))
 
-            # Put the batch first.
-            context_sentences_to_encode = context_sentences_to_encode.permute(1, 0, 2).contiguous()
 
             if torch.cuda.is_available():
                 context_sentences_to_encode = context_sentences_to_encode.cuda()
 
-            #print("Context", context_sentences_to_encode.size())
+            # print("Context", context_sentences_to_encode.size())
             encoded_passages, _ = self._model.encode_passages(context_sentences_to_encode)
             encoded_passages = torch.squeeze(encoded_passages, dim=0)
 
-            #print("Encoded passages", encoded_passages.size())
+            print("Encoded passages", encoded_passages.size())
 
             encoded_passages = encoded_passages.cpu()
 
             encoded_passages_list.append(encoded_passages)
         encoded_passages_all_tensor = torch.stack(encoded_passages_list)
 
-        #print(f"Passages before {encoded_passages_all_tensor.size()}")
-        encoded_passages_all_tensor = encoded_passages_all_tensor.view(
-            (encoded_passages_all_tensor.size(0) * encoded_passages_all_tensor.size(1),
-             encoded_passages_all_tensor.size(2), encoded_passages_all_tensor.size(3)))
-
         print(f"Passages after {encoded_passages_all_tensor.size()}")
 
-        context_encoded_representation = encoded_passages_all_tensor[0, -2, ...]
+        context_encoded_representation = encoded_passages_all_tensor[0, -2, : ]
         final_encoded_representations = encoded_passages_all_tensor[:, -1, :]
 
         return context_encoded_representation, final_encoded_representations
@@ -611,6 +597,10 @@ class KnowledgeablePredictor(Predictor):
             encoded_sentences.append(encoded_sentences_batch)
 
         encoded_sentences_tensor = torch.stack(encoded_sentences, dim=0)
+
+        encoded_sentences_tensor = encoded_sentences_tensor.view(
+            encoded_sentences_tensor.size(1) * encoded_sentences_tensor.size(2), encoded_sentences_tensor.size(3),
+            encoded_sentences_tensor.size(4))
 
         return encoded_sentences_tensor
 
