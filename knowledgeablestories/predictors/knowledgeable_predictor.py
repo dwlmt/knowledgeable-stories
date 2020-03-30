caaedfa0708de9d3503e3c0829104b2cc12e3f2a
David Wilmot <david.wilmot@gmail.com>
2020-03-30 03:28:35 +0100

    Try on the cpu.

--- knowledgeablestories/predictors/knowledgeable_predictor.py
+++ knowledgeablestories/predictors/knowledgeable_predictor.py
@@ -561,7 +561,7 @@ class KnowledgeablePredictor(Predictor):
                 (existing_sentences_expanded, torch.unsqueeze(encoded_sentences_batch, dim=1)), dim=1).contiguous()
 
             # print("Context", context_sentences_to_encode.size())
-            encoded_passages, _ = self._model.encode_passages(context_sentences_to_encode)
+            encoded_passages, _ = self._model.encode_passages(context_sentences_to_encode.cpu())
 
             encoded_passages = encoded_passages.cpu()
             encoded_sentences_batch = encoded_sentences_batch.cpu()
