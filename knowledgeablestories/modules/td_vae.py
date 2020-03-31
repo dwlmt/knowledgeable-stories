fdebdfece48fc653b81e30ea40a3215ef7a1f7bf
David Wilmot <david.wilmot@gmail.com>
2020-03-31 19:18:02 +0100

    Refactor the TD-VAE code splitting common functionality in methods, and renaming to be clearer.

--- knowledgeablestories/modules/td_vae.py
+++ knowledgeablestories/modules/td_vae.py
@@ -1,8 +1,9 @@
-"""Placeholder, adapted from https://github.com/ankitkv/TD-VAE"""
+""" Adapted from https://github.com/ankitkv/TD-VAE"""
 import numpy
 import torch
 from torch import nn
 from torch.nn import functional as F
+
 
 class DBlock(nn.Module):
     """ A basic building block for computing parameters of a normal distribution.
@@ -21,21 +22,6 @@ class DBlock(nn.Module):
         mu = self.fc_mu(t)
         logsigma = self.fc_logsigma(t)
         return mu, logsigma
-
-
-class PreProcess(nn.Module):
-    """ The pre-process layer for MNIST image.
-    """
-
-    def __init__(self, input_size, processed_x_size):
-        super().__init__()
-        self.fc1 = nn.Linear(input_size, processed_x_size)
-        self.fc2 = nn.Linear(processed_x_size, processed_x_size)
-
-    def forward(self, input_):
-        t = torch.relu(self.fc1(input_))
-        t = torch.relu(self.fc2(t))
-        return t
 
 
 class Decoder(nn.Module):
@@ -59,179 +45,185 @@ class TDVAE(nn.Module):
     """ The full TD-VAE model with jumpy prediction.
     """
 
-    def __init__(self, x_size, processed_x_size, b_size, z_size, layers, samples_per_seq, t_diff_min, t_diff_max):
+    def __init__(self, x_size, input_size, belief_size, z_posterior_size, num_layers, samples_per_seq, t_diff_min,
+                 t_diff_max):
         super().__init__()
-        self.layers = layers
+        self.num_layers = num_layers
         self.samples_per_seq = samples_per_seq
         self.t_diff_min = t_diff_min
         self.t_diff_max = t_diff_max
 
-        x_size = x_size
-        processed_x_size = processed_x_size
-        b_size = b_size
-        z_size = z_size
-
-        # Input pre-process layer
-        self.process_x = PreProcess(x_size, processed_x_size)
-
         # Multilayer LSTM for aggregating belief states
-        self.b_rnn = ops.MultilayerLSTM(input_size=processed_x_size, hidden_size=b_size, layers=layers,
-                                        every_layer_input=True, use_previous_higher=True)
+        self.b_belief_rnn = nn.LSTM(input_size=input_size, hidden_size=belief_size, num_layers=num_layers)
 
         # Multilayer state model is used. Sampling is done by sampling higher layers first.
-        self.z_b = nn.ModuleList([DBlock(b_size + (z_size if layer < layers - 1 else 0), 50, z_size)
-                                  for layer in range(layers)])
+        self.z_posterior_belief = nn.ModuleList(
+            [DBlock(belief_size + (z_posterior_size if layer < num_layers - 1 else 0), 50, z_posterior_size)
+             for layer in range(num_layers)])
 
         # Given belief and state at time t2, infer the state at time t1
-        self.z1_z2_b1 = nn.ModuleList([DBlock(b_size + layers * z_size + (z_size if layer < layers - 1 else 0), 50,
-                                              z_size) for layer in range(layers)])
+        self.z1_z2_b1_inference = nn.ModuleList([DBlock(
+            belief_size + num_layers * z_posterior_size + (z_posterior_size if layer < num_layers - 1 else 0), 50,
+            z_posterior_size) for layer in range(num_layers)])
 
         # Given the state at time t1, model state at time t2 through state transition
-        self.z2_z1 = nn.ModuleList([DBlock(layers * z_size + (z_size if layer < layers - 1 else 0), 50, z_size)
-                                    for layer in range(layers)])
+        self.z2_z1_prediction = nn.ModuleList([DBlock(
+            num_layers * z_posterior_size + (z_posterior_size if layer < num_layers - 1 else 0), 50, z_posterior_size)
+            for layer in range(num_layers)])
 
         # state to observation
-        self.x_z = Decoder(layers * z_size, 200, x_size)
+        self.x_z_decoder = Decoder(num_layers * z_posterior_size, 200, x_size)
 
-    def forward(self, x):
-        # sample t1 and t2
+    def forward(self, x, mask=None):
+        # TODO mask so does not go beyond the length of the batch.
+
+        # Sample the current and future time points.
         t1 = torch.randint(0, x.size(1) - self.t_diff_max, (self.samples_per_seq, x.size(0)), device=x.device)
         t2 = t1 + torch.randint(self.t_diff_min, self.t_diff_max + 1, (self.samples_per_seq, x.size(0)),
                                 device=x.device)
-        # x = x[:, :t2.max() + 1]  # usually not required with big enough batch size
 
-        # pre-process image x
-        processed_x = self.process_x(x)  # max x length is max(t2) + 1
+        # Truncate the sequence if not required to the end.
+        x = x[:, :t2.max() + 1]
 
-        # aggregate the belief b
-        b = self.b_rnn(processed_x)  # size: bs, time, layers, dim
+        # Run LSTM to get belief states.
+        b1, b2 = self._beliefs(x, t1, t2)
 
-        # replicate b multiple times
-        b = b[None, ...].expand(self.samples_per_seq, -1, -1, -1, -1)  # size: copy, bs, time, layers, dim
+        # Sample the z posteriors (or states of the world for b1 and b2)
+        qb_z2_b2_mu, qb_z2_b2_logvar, qb_z2_b2, qb_z2_b2_mus, qb_z2_b2_logvars, qb_z2_b2s = self.sample_posterior_z(b2)
+        pb_z1_b1_mu, pb_z1_b1_logvar, pb_z1_b1, pb_z1_b1_mus, pb_z1_b1_logvars, pb_z1_b1s = self.sample_posterior_z(b1)
 
-        # Element-wise indexing. sizes: bs, layers, dim
-        b1 = torch.gather(b, 2, t1[..., None, None, None].expand(-1, -1, -1, b.size(3), b.size(4))).view(
-            -1, b.size(3), b.size(4))
-        b2 = torch.gather(b, 2, t2[..., None, None, None].expand(-1, -1, -1, b.size(3), b.size(4))).view(
-            -1, b.size(3), b.size(4))
+        # Infer the smoothing distibution, i.e. the state of the world at b1.
+        qs_z1_z2_b1, qs_z1_z2_b1_logvar, qs_z1_z2_b1_mu, qs_z1_z2_b1s = self._q_inference(b1, qb_z2_b2)
 
-        # q_B(z2 | b2)
-        qb_z2_b2_mus, qb_z2_b2_logvars, qb_z2_b2s = [], [], []
-        for layer in range(self.layers - 1, -1, -1):
-            if layer == self.layers - 1:
-                qb_z2_b2_mu, qb_z2_b2_logvar = self.z_b[layer](b2[:, layer])
+        # Predict the future z from the smoothing distribution and lower level b state.
+        pt_z2_z1_logvar, pt_z2_z1_mu = self._z_prediction(qb_z2_b2s, qs_z1_z2_b1)
+
+        # This decoder is grounding the prediction in the data x2 from t2.
+        pd_x2_z2 = self.x_z_decoder(qb_z2_b2)
+
+        return (x, t2, qs_z1_z2_b1_mu, qs_z1_z2_b1_logvar, pb_z1_b1_mu, pb_z1_b1_logvar, qb_z2_b2_mu, qb_z2_b2_logvar,
+                qb_z2_b2, pt_z2_z1_mu, pt_z2_z1_logvar, pd_x2_z2)
+
+    def _z_prediction(self, qb_z2_b2s, qs_z1_z2_b1):
+        ''' Predicts forwards to z2 from z1.
+            p_T(z2 | z1), also conditions on q_B(z2) from higher layer
+        '''
+        pt_z2_z1_mus, pt_z2_z1_logvars = [], []
+        for layer in range(self.num_layers - 1, -1, -1):
+            if layer == self.num_layers - 1:
+                pt_z2_z1_mu, pt_z2_z1_logvar = self.z2_z1_prediction[layer](qs_z1_z2_b1)
             else:
-                qb_z2_b2_mu, qb_z2_b2_logvar = self.z_b[layer](torch.cat([b2[:, layer], qb_z2_b2], dim=1))
-            qb_z2_b2_mus.insert(0, qb_z2_b2_mu)
-            qb_z2_b2_logvars.insert(0, qb_z2_b2_logvar)
+                pt_z2_z1_mu, pt_z2_z1_logvar = self.z2_z1_prediction[layer](
+                    torch.cat([qs_z1_z2_b1, qb_z2_b2s[layer + 1]], dim=1))
+            pt_z2_z1_mus.insert(0, pt_z2_z1_mu)
+            pt_z2_z1_logvars.insert(0, pt_z2_z1_logvar)
+        pt_z2_z1_mu = torch.cat(pt_z2_z1_mus, dim=1)
+        pt_z2_z1_logvar = torch.cat(pt_z2_z1_logvars, dim=1)
+        return pt_z2_z1_logvar, pt_z2_z1_mu
 
-            qb_z2_b2 = reparameterize_gaussian(qb_z2_b2_mu, qb_z2_b2_logvar, self.training)
-            qb_z2_b2s.insert(0, qb_z2_b2)
-
-        qb_z2_b2_mu = torch.cat(qb_z2_b2_mus, dim=1)
-        qb_z2_b2_logvar = torch.cat(qb_z2_b2_logvars, dim=1)
-        qb_z2_b2 = torch.cat(qb_z2_b2s, dim=1)
-
-        # q_S(z1 | z2, b1, b2) ~= q_S(z1 | z2, b1)
+    def _q_inference(self, b1, qb_z2_b2):
+        ''' This is the backwards q inference from the paper.
+        The projection from t2 back to the state of the world at t1.
+          q_S(z1 | z2, b1, b2) ~= q_S(z1 | z2, b1)
+        '''
         qs_z1_z2_b1_mus, qs_z1_z2_b1_logvars, qs_z1_z2_b1s = [], [], []
-        for layer in range(self.layers - 1, -1, -1):  # TODO optionally condition t2 - t1
-            if layer == self.layers - 1:
-                qs_z1_z2_b1_mu, qs_z1_z2_b1_logvar = self.z1_z2_b1[layer](torch.cat([qb_z2_b2, b1[:, layer]], dim=1))
+        for layer in range(self.num_layers - 1, -1, -1):
+            if layer == self.num_layers - 1:
+                qs_z1_z2_b1_mu, qs_z1_z2_b1_logvar = self.z1_z2_b1_inference[layer](
+                    torch.cat([qb_z2_b2, b1[:, layer]], dim=1))
             else:
-                qs_z1_z2_b1_mu, qs_z1_z2_b1_logvar = self.z1_z2_b1[layer](torch.cat([qb_z2_b2, b1[:, layer],
-                                                                                     qs_z1_z2_b1], dim=1))
+                qs_z1_z2_b1_mu, qs_z1_z2_b1_logvar = self.z1_z2_b1_inference[layer](torch.cat([qb_z2_b2, b1[:, layer],
+                                                                                               qs_z1_z2_b1], dim=1))
             qs_z1_z2_b1_mus.insert(0, qs_z1_z2_b1_mu)
             qs_z1_z2_b1_logvars.insert(0, qs_z1_z2_b1_logvar)
 
             qs_z1_z2_b1 = reparameterize_gaussian(qs_z1_z2_b1_mu, qs_z1_z2_b1_logvar, self.training)
             qs_z1_z2_b1s.insert(0, qs_z1_z2_b1)
-
         qs_z1_z2_b1_mu = torch.cat(qs_z1_z2_b1_mus, dim=1)
         qs_z1_z2_b1_logvar = torch.cat(qs_z1_z2_b1_logvars, dim=1)
         qs_z1_z2_b1 = torch.cat(qs_z1_z2_b1s, dim=1)
+        return qs_z1_z2_b1, qs_z1_z2_b1_logvar, qs_z1_z2_b1_mu, qs_z1_z2_b1s
 
-        # p_T(z2 | z1), also conditions on q_B(z2) from higher layer
-        pt_z2_z1_mus, pt_z2_z1_logvars = [], []
-        for layer in range(self.layers - 1, -1, -1):  # TODO optionally condition t2 - t1
-            if layer == self.layers - 1:
-                pt_z2_z1_mu, pt_z2_z1_logvar = self.z2_z1[layer](qs_z1_z2_b1)
+    def sample_posterior_z(self, b):
+        ''' Samples the posterior for a belief.
+        '''
+        z_b_mus, z_b_logvars, b_z_bs = [], [], []
+        for layer in range(self.num_layers - 1, -1, -1):
+            if layer == self.num_layers - 1:
+                z_b_mu, z_b_logvar = self.z_posterior_belief[layer](b[:, layer])
             else:
-                pt_z2_z1_mu, pt_z2_z1_logvar = self.z2_z1[layer](torch.cat([qs_z1_z2_b1, qb_z2_b2s[layer + 1]], dim=1))
-            pt_z2_z1_mus.insert(0, pt_z2_z1_mu)
-            pt_z2_z1_logvars.insert(0, pt_z2_z1_logvar)
+                z_b_mu, z_b_logvar = self.z_posterior_belief[layer](
+                    torch.cat([b[:, layer], z_b], dim=1))
+            z_b_mus.insert(0, z_b_mu)
+            z_b_logvars.insert(0, z_b_logvar)
 
-        pt_z2_z1_mu = torch.cat(pt_z2_z1_mus, dim=1)
-        pt_z2_z1_logvar = torch.cat(pt_z2_z1_logvars, dim=1)
+            z_b = reparameterize_gaussian(z_b_mu, z_b_logvar, self.training)
+            b_z_bs.insert(0, z_b)
 
-        # p_B(z1 | b1)
-        pb_z1_b1_mus, pb_z1_b1_logvars = [], []
-        for layer in range(self.layers - 1, -1, -1):  # TODO optionally condition t2 - t1
-            if layer == self.layers - 1:
-                pb_z1_b1_mu, pb_z1_b1_logvar = self.z_b[layer](b1[:, layer])
-            else:
-                pb_z1_b1_mu, pb_z1_b1_logvar = self.z_b[layer](torch.cat([b1[:, layer], qs_z1_z2_b1s[layer + 1]],
-                                                                         dim=1))
-            pb_z1_b1_mus.insert(0, pb_z1_b1_mu)
-            pb_z1_b1_logvars.insert(0, pb_z1_b1_logvar)
+        z_b_mu = torch.cat(z_b_mus, dim=1)
+        z_b_logvar = torch.cat(z_b_logvars, dim=1)
+        b_z_b = torch.cat(b_z_bs, dim=1)
 
-        pb_z1_b1_mu = torch.cat(pb_z1_b1_mus, dim=1)
-        pb_z1_b1_logvar = torch.cat(pb_z1_b1_logvars, dim=1)
+        return z_b_mu, z_b_logvar, b_z_b, z_b_mus, z_b_logvars, b_z_bs
 
-        # p_D(x2 | z2)
-        pd_x2_z2 = self.x_z(qb_z2_b2)
-
-        return (x, t2, qs_z1_z2_b1_mu, qs_z1_z2_b1_logvar, pb_z1_b1_mu, pb_z1_b1_logvar, qb_z2_b2_mu, qb_z2_b2_logvar,
-                qb_z2_b2, pt_z2_z1_mu, pt_z2_z1_logvar, pd_x2_z2)
-
-    def visualize(self, x, t, n):
-        # pre-process image x
-        processed_x = self.process_x(x)  # x length is t + 1
-
+    def _beliefs(self, x, t1, t2):
+        ''' Runs the LSTMS to obtain beliefs at t1 and t2
+        '''
         # aggregate the belief b
-        b = self.b_rnn(processed_x)[:, t]  # size: bs, time, layers, dim
+        b = self.b_belief_rnn(x)  # size: bs, time, layers, dim
+        # replicate b multiple times
+        b = b[None, ...].expand(self.samples_per_seq, -1, -1, -1, -1)  # size: copy, bs, time, layers, dim
+        # Element-wise indexing. sizes: bs, layers, dim
+        b1 = torch.gather(b, 2, t1[..., None, None, None].expand(-1, -1, -1, b.size(3), b.size(4))).view(
+            -1, b.size(3), b.size(4))
+        b2 = torch.gather(b, 2, t2[..., None, None, None].expand(-1, -1, -1, b.size(3), b.size(4))).view(
+            -1, b.size(3), b.size(4))
+        return b1, b2
 
-        # compute z from b
-        p_z_bs = []
-        for layer in range(self.layers - 1, -1, -1):
-            if layer == self.layers - 1:
-                p_z_b_mu, p_z_b_logvar = self.z_b[layer](b[:, layer])
-            else:
-                p_z_b_mu, p_z_b_logvar = self.z_b[layer](torch.cat([b[:, layer], p_z_b], dim=1))
-            p_z_b = reparameterize_gaussian(p_z_b_mu, p_z_b_logvar, True)
-            p_z_bs.insert(0, p_z_b)
+    def rollout_posteriors(self, x, t, n):
 
-        z = torch.cat(p_z_bs, dim=1)
+        # Run belief network
+        b = self.b_belief_rnn(x)[:, t]  # size: bs, time, layers, dim
+
+        # Compute posterior, state of the world from belief.
+        _, _, z, _, _, _ = self.sample_posterior_z(b)
+
+        # Rollout for n timesteps predicting the future zs at n.
         rollout_x = []
-
         for _ in range(n):
             next_z = []
-            for layer in range(self.layers - 1, -1, -1):  # TODO optionally condition n
-                if layer == self.layers - 1:
-                    pt_z2_z1_mu, pt_z2_z1_logvar = self.z2_z1[layer](z)
+            for layer in range(self.num_layers - 1, -1, -1):
+                if layer == self.num_layers - 1:
+                    pt_z2_z1_mu, pt_z2_z1_logvar = self.z2_z1_prediction[layer](z)
                 else:
-                    pt_z2_z1_mu, pt_z2_z1_logvar = self.z2_z1[layer](torch.cat([z, pt_z2_z1], dim=1))
+                    pt_z2_z1_mu, pt_z2_z1_logvar = self.z2_z1_prediction[layer](torch.cat([z, pt_z2_z1], dim=1))
                 pt_z2_z1 = reparameterize_gaussian(pt_z2_z1_mu, pt_z2_z1_logvar, True)
                 next_z.insert(0, pt_z2_z1)
 
             z = torch.cat(next_z, dim=1)
-            rollout_x.append(self.x_z(z))
+            rollout_x.append(self.x_z_decoder(z))
 
         return torch.stack(rollout_x, dim=1)
 
     def loss_function(self, forward_ret, labels=None):
+        ''' Takes the output from the main forward.
+        '''
         (x, t2, qs_z1_z2_b1_mu, qs_z1_z2_b1_logvar, pb_z1_b1_mu, pb_z1_b1_logvar, qb_z2_b2_mu, qb_z2_b2_logvar,
          qb_z2_b2, pt_z2_z1_mu, pt_z2_z1_logvar, pd_x2_z2) = forward_ret
 
-        # replicate x multiple times
-        x = x[None, ...].expand(self.flags.samples_per_seq, -1, -1, -1)  # size: copy, bs, time, dim
+        # Copy and expand x.
+        x = x[None, ...].expand(self.samples_per_seq, -1, -1, -1)  # size: copy, bs, time, dim
         x2 = torch.gather(x, 2, t2[..., None, None].expand(-1, -1, -1, x.size(3))).view(-1, x.size(3))
         batch_size = x2.size(0)
 
+        # Minimize the loss between smoothed q and current beliefs.
         kl_div_qs_pb = kl_div_gaussian(qs_z1_z2_b1_mu, qs_z1_z2_b1_logvar, pb_z1_b1_mu, pb_z1_b1_logvar).mean()
 
+        # Predict Z2 from Z1 as if it has all the future information by known at t2.
         kl_shift_qb_pt = (gaussian_log_prob(qb_z2_b2_mu, qb_z2_b2_logvar, qb_z2_b2) -
                           gaussian_log_prob(pt_z2_z1_mu, pt_z2_z1_logvar, qb_z2_b2)).mean()
 
+        # Ground the t2 state of the world in the data, reconstruction loss.
         bce = F.binary_cross_entropy(pd_x2_z2, x2, reduction='sum') / batch_size
         bce_optimal = F.binary_cross_entropy(x2, x2, reduction='sum').detach() / batch_size
         bce_diff = bce - bce_optimal
@@ -253,10 +245,12 @@ def reparameterize_gaussian(mu, logvar, sample, return_eps=False):
     else:
         return ret
 
+
 def gaussian_log_prob(mu, logvar, x):
     '''Batched log probability log p(x) computation.'''
-    logprob = -0.5 * (torch.log(2.0 * numpy.pi) + logvar + ((x - mu)**2 / logvar.exp()))
+    logprob = -0.5 * (torch.log(2.0 * numpy.pi) + logvar + ((x - mu) ** 2 / logvar.exp()))
     return logprob.sum(dim=-1)
+
 
 def kl_div_gaussian(q_mu, q_logvar, p_mu=None, p_logvar=None):
     '''Batched KL divergence D(q||p) computation.'''
@@ -265,5 +259,5 @@ def kl_div_gaussian(q_mu, q_logvar, p_mu=None, p_logvar=None):
         p_mu = p_mu or zero
         p_logvar = p_logvar or zero
     logvar_diff = q_logvar - p_logvar
-    kl_div = -0.5 * (1.0 + logvar_diff - logvar_diff.exp() - ((q_mu - p_mu)**2 / p_logvar.exp()))
+    kl_div = -0.5 * (1.0 + logvar_diff - logvar_diff.exp() - ((q_mu - p_mu) ** 2 / p_logvar.exp()))
     return kl_div.sum(dim=-1)
