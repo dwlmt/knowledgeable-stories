3f451c96608ca1fe62acac03893e9f717c5da3e0
David Wilmot <david.wilmot@gmail.com>
2020-04-06 14:58:32 +0100

    Print output.

--- knowledgeablestories/models/knowledgeable_story_model.py
+++ knowledgeablestories/models/knowledgeable_story_model.py
@@ -218,8 +218,8 @@ class KnowledgeableStoriesModel(Model):
                         self.encode_passages(encoded_sentences, passage_mask)
 
                     if "passage_disc_loss" in self._loss_weights:
-                        # if self._passage_to_lm_encoder is not None:
-                        #    encoded_sentences = self._passage_to_lm_encoder(encoded_sentences)
+                        if self._passage_to_lm_encoder is not None:
+                            encoded_sentences = self._passage_to_lm_encoder(encoded_sentences)
 
                         passage_disc_loss, disc_output_dict = self._calculate_disc_loss(passages_encoded,
                                                                                         encoded_sentences,
@@ -324,6 +324,7 @@ class KnowledgeableStoriesModel(Model):
                         self._bleu_score_if_required(dataset_name, prem_tokens, conclusions, generated_text)
 
         output["loss"] = loss
+        print(output)
 
         return output
 
