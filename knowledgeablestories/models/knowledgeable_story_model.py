5f299f19c55dfb29deb56a727d6e0a383716f916
David Wilmot <david.wilmot@gmail.com>
2020-04-15 19:14:10 +0100

    Apply fusion.

--- knowledgeablestories/models/knowledgeable_story_model.py
+++ knowledgeablestories/models/knowledgeable_story_model.py
@@ -259,6 +259,7 @@ class KnowledgeableStoriesModel(Model):
                             self._metrics["passage_disc_loss"](passage_disc_loss.item())
                         elif fusion and self._fusion_dense is not None:
 
+                            print(lm_output.size(), passages_encoded.size())
                             fused_tokens = self._fusion_dense(torch.cat((lm_output, passages_encoded), dim=-1))
 
                             self._lm_model = self._lm_model.to(fused_tokens.device)
