fb5d4264046ebd7b8280416e473d8dac8fae4115
David Wilmot <david.wilmot@gmail.com>
2020-04-18 21:48:02 +0100

    First attempt at a transformer encoder that will use average pooling on top of new transformer layer.

--- knowledgeablestories/modules/sentence_pooler.py
+++ knowledgeablestories/modules/sentence_pooler.py
@@ -1,20 +1,19 @@
 import torch
 from allennlp.modules import Seq2SeqEncoder
-from allennlp.modules.seq2vec_encoders import BagOfEmbeddingsEncoder
 from allennlp.modules.seq2vec_encoders.seq2vec_encoder import Seq2VecEncoder
 from overrides import overrides
 
 
-@Seq2VecEncoder.register("sentence_pooler")
+@Seq2VecEncoder.register("seq2seq_pooler")
 class BagOfEmbeddingsEncoder(Seq2VecEncoder):
     """
         Simply combine a seq2seq encoder with a boe pooler to average across the underlying seq2seq model.
     """
 
-    def __init__(self, seq2seq_encoder: Seq2SeqEncoder, boe: BagOfEmbeddingsEncoder) -> None:
+    def __init__(self, seq2seq_encoder: Seq2SeqEncoder, pooler: Seq2VecEncoder) -> None:
         super().__init__()
         self._seq2seq_encoder = seq2seq_encoder
-        self._boe = boe
+        self._pooler = pooler
 
     @overrides
     def get_input_dim(self) -> int:
@@ -22,9 +21,9 @@ class BagOfEmbeddingsEncoder(Seq2VecEncoder):
 
     @overrides
     def get_output_dim(self) -> int:
-        return self._boe.get_output_dim
+        return self._pooler.get_output_dim
 
     def forward(self, tokens: torch.Tensor, mask: torch.BoolTensor = None):
         seq_output = self._seq2seq_encoder(tokens, mask=mask)
-        pooled_output = self._boe(seq_output, mask=mask)
+        pooled_output = self._pooler(seq_output, mask=mask)
         return pooled_output
