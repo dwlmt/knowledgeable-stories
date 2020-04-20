74e33188aa50168872901e05ae377d65c3c9387f
David Wilmot <david.wilmot@gmail.com>
2020-04-20 23:54:56 +0100

    Change TDVAE so returns samples for output as well.

--- knowledgeablestories/modules/td_vae.py
+++ knowledgeablestories/modules/td_vae.py
@@ -211,7 +211,7 @@ class TDVAE(nn.Module, FromParams):
             -1, b.size(3), b.size(4))
         return b1, b2
 
-    def rollout_posteriors_sequence(self, x, n: int = None, samples: int = None):
+    def rollout_posteriors_sequence(self, x, n: int = None, do_sample: bool = False):
         ''' Calculate the rollout for all the ns in the sequence.
         '''
         if n == None:
@@ -224,7 +224,7 @@ class TDVAE(nn.Module, FromParams):
 
         batch_size, seq_size, embedding_size = x.size()
         for i in range(seq_size):
-            rollout_x, rollout_z2, z1, b = self.rollout_posteriors(x, t=i, n=n)
+            rollout_x, rollout_z2, z1, b = self.rollout_posteriors(x, t=i, n=n, do_sample=do_sample)
             rollout_xs.append(rollout_x)
             rollout_z2s.append(rollout_z2)
             z1s.append(z1)
@@ -235,9 +235,15 @@ class TDVAE(nn.Module, FromParams):
         z1s = torch.stack(z1s)
         bs = torch.stack(bs)
 
-        rollout_z2s = rollout_z2s.view(rollout_z2s.size(0), rollout_z2s.size(1), self.num_layers,
-                                       int(rollout_z2s.size(2) / self.num_layers))
-        z1s = z1s.view(z1s.size(0), self.num_layers, int(z1s.size(1) / self.num_layers))
+        if not do_sample:
+            rollout_z2s = rollout_z2s.view(rollout_z2s.size(0), rollout_z2s.size(1), self.num_layers,
+                                           int(rollout_z2s.size(2) / self.num_layers))
+            z1s = z1s.view(z1s.size(0), self.num_layers, int(z1s.size(1) / self.num_layers))
+        else:
+            rollout_z2s = rollout_z2s.view(rollout_z2s.size(0), rollout_z2s.size(1), rollout_z2s.size(2),
+                                           self.num_layers,
+                                           int(rollout_z2s.size(3) / self.num_layers))
+            z1s = z1s.view(z1s.size(0), z1s.size(1), self.num_layers, int(z1s.size(2) / self.num_layers))
 
         print("TD-VAE Rollout return vectors", rollout_xs.size(), rollout_z2s.size(), z1s.size(), bs.size())
 
