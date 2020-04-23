a965e2d4f4447220eb2a8f67ba0cc4e483c473df
David Wilmot <david.wilmot@gmail.com>
2020-04-23 23:33:45 +0100

    Start the offsets at one so offsets are correct in predictions.

--- knowledgeablestories/predictors/knowledgeable_predictor.py
+++ knowledgeablestories/predictors/knowledgeable_predictor.py
@@ -299,7 +299,7 @@ class KnowledgeablePredictor(Predictor):
                 z1 = curr_sampled_z1[i][0]
 
                 for k, (z1_layer, z2_layer) in enumerate(zip(z1, z2)):
-                    print(k, z1_layer.size(), z2_layer.size())
+                    # print(k, z1_layer.size(), z2_layer.size())
                     l1 = torch.mean(self._l1_distance(z1_layer, z2_layer), dim=-1)
                     l2 = torch.mean(self._l2_distance(z1_layer, z2_layer), dim=-1)
                     cosine = 1.0 - torch.mean(self._cosine_similarity(z1_layer, z2_layer), dim=-1)
