db4779be89ed850c22ec869e767d27c8e04df61f
David Wilmot <david.wilmot@gmail.com>
2020-04-27 12:20:14 +0100

    Comment out permute on batch norms.

--- knowledgeablestories/modules/sentence_pooler.py
+++ knowledgeablestories/modules/sentence_pooler.py
@@ -44,9 +44,9 @@ class PoolingEncoder(Seq2VecEncoder):
         pooled_output = self._pooler(seq_output, mask=mask)
         print("Pooled Output Size", pooled_output.size())
 
-        pooled_output = pooled_output.permute(0, 2, 1)
+        # pooled_output = pooled_output.permute(0, 2, 1)
         pooled_output = self._pooler_batch_norm(pooled_output)
-        pooled_output = pooled_output.permute(0, 2, 1)
+        # pooled_output = pooled_output.permute(0, 2, 1)
         # print("Pooled Output", pooled_output[torch.isnan(pooled_output)].size())
 
         # pooled_output = torch.where(torch.isnan(pooled_output), torch.zeros_like(pooled_output), pooled_output)
