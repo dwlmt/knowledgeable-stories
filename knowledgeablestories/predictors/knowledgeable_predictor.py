e2dd6d514746ceca8f13808135b1dd06911ea345
David Wilmot <david.wilmot@gmail.com>
2020-05-26 00:21:04 +0100

    Cat fusion layer.

--- knowledgeablestories/predictors/knowledgeable_predictor.py
+++ knowledgeablestories/predictors/knowledgeable_predictor.py
@@ -1011,7 +1011,7 @@ class KnowledgeablePredictor(Predictor):
 
             if passages_encoded is not None:
                 print("Passages encoded sizes", next_token_hidden.size(), passages_encoded.size())
-                next_token_hidden = self._model._fusion_dense(next_token_hidden, passages_encoded[-1])
+                next_token_hidden = self._model._fusion_dense(torch.cat((torch.unsqueeze(next_token_hidden, dim=-1), passages_encoded[-1])),dim=-1)
 
             next_token_logits = self._model._lm_model.lm_head(next_token_hidden)
 
