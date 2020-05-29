fa4fada05e97aac00f7c929e1b0cc716f7b4193d
David Wilmot <david.wilmot@gmail.com>
2020-05-29 21:15:09 +0100

    Just simply average the accuracies and write to an output file.

--- scripts/evaluate_tdvae_cloze.py
+++ scripts/evaluate_tdvae_cloze.py
@@ -1,11 +1,47 @@
+import csv
 
 import fire
+from jsonlines import jsonlines
+from tqdm import tqdm
+
 
 class EvalTdvaeCloze(object):
-  """Evaluate TDVAE Cloze"""
+    """Evaluate TDVAE Cloze
 
-  def eval(self, prediction_json: str, output_file: str):
-    print(f"Evaluate TDVAE Cloze from {prediction_json}, to {output_file}")
+    """
+    def eval(self, prediction_json: str, output_file: str):
+        """ Evaluate TDVAE
+
+            @param prediction_json: Source with the prediction json data.
+            @param output_file
+        """
+        print(f"Evaluate TDVAE Cloze from {prediction_json}, to {output_file}")
+
+        with jsonlines.open(prediction_json) as reader:
+
+            total_rows = 0
+            sum_dict = {}
+            for i, obj in tqdm(enumerate(reader)):
+
+                if i == 0:
+                    sum_dict = obj["accuracy"]
+                else:
+                    accuracy = obj["accuracy"]
+                    sum_dict = {k: sum_dict.get(k, 0) + accuracy.get(k, 0) for k in set(sum_dict) | set(accuracy)}
+
+                total_rows += 1
+
+        accuracy_dict = {}
+        for k, v in sum_dict.items():
+            accuracy_dict[k] = float(v) / float(total_rows)
+
+        with open(output_file, 'w') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(["metric", "accuracy"])
+
+            for k, v in accuracy_dict:
+                csv_writer.writerow([k, v])
+
 
 if __name__ == '__main__':
-  fire.Fire(EvalTdvaeCloze)
+    fire.Fire(EvalTdvaeCloze)
