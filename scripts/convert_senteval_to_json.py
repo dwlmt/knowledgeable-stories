4e004c5185bb9c3d6508a955aaf0268ed3cef9be
David Wilmot <david.wilmot@gmail.com>
2020-06-04 19:35:25 +0100

    Simple convert script senteval.

---
+++ scripts/convert_senteval_to_json.py
@@ -0,0 +1,19 @@
+import csv
+
+import fire
+from jsonlines import jsonlines
+from tqdm import tqdm
+
+
+class ConvertSentEval(object):
+    def convert(self, source: str, target: str):
+
+        print(f"Convert from text to json: {source}, to {target}")
+
+        with open(source) as reader:
+            with jsonlines.open(target, mode='w') as writer:
+                for line in reader:
+                    writer.write({"text": line})
+
+if __name__ == '__main__':
+    fire.Fire(ConvertSentEval)
