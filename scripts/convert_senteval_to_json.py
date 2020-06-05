2c1f62a52e94491bac9285d3203255727d93c0d2
David Wilmot <david.wilmot@gmail.com>
2020-06-05 10:17:25 +0100

    Convert sentences to story format.

--- scripts/convert_senteval_to_json.py
+++ scripts/convert_senteval_to_json.py
@@ -1,19 +1,33 @@
 import csv
 
 import fire
+import more_itertools
 from jsonlines import jsonlines
 from tqdm import tqdm
 
 
 class ConvertSentEval(object):
-    def convert(self, source: str, target: str):
+    def convert(self, source: str, target: str, batch_size: int = 100):
 
         print(f"Convert from text to json: {source}, to {target}")
 
         with open(source) as reader:
+            all_lines = list(reader.readlines())
             with jsonlines.open(target, mode='w') as writer:
-                for line in reader:
-                    writer.write({"text": line.strip('\n')})
+
+                for batch_id, batch in enumerate(more_itertools.chunked(all_lines, batch_size)):
+
+                    story_dict = {}
+                    story_dict["story_id"] = batch_id
+
+                    sentences = []
+
+                    for j, sentence in enumerate(sentences):
+                        sentences.append({"sentence_num": j,"text": sentence.strip('\n')})
+
+                    story_dict["sentences"] = sentences
+
+                    writer.write(story_dict)
 
 if __name__ == '__main__':
     fire.Fire(ConvertSentEval)
