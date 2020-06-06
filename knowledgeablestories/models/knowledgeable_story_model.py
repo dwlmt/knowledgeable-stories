dc020799fbbf2b3da5d32aa50e716b1139b2fd0a
David Wilmot <david.wilmot@gmail.com>
2020-06-06 20:21:02 +0100

    print encoded sizes.

--- knowledgeablestories/models/knowledgeable_story_model.py
+++ knowledgeablestories/models/knowledgeable_story_model.py
@@ -742,6 +742,7 @@ class KnowledgeableStoriesModel(Model):
         source_encoded = source_encoded.clone() * mask_expanded
         target_encoded = target_encoded.clone() * mask_expanded
 
+        print("Encoded views", source_encoded.size(), target_encoded.size())
         source_encoded_flat = source_encoded.view(batch_size * sentence_num, feature_size)
         target_encoded_flat = target_encoded.view(batch_size * sentence_num, feature_size)
 
