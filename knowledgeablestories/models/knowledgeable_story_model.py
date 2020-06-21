2220fc4916f98be49f2c2a0f765deadaef6ca440
David Wilmot <david.wilmot@gmail.com>
2020-06-21 18:16:41 +0100

    Checks so generated sentences are always added.

--- knowledgeablestories/models/knowledgeable_story_model.py
+++ knowledgeablestories/models/knowledgeable_story_model.py
@@ -1100,28 +1100,27 @@ class KnowledgeableStoriesModel(Model):
 
                 generated_sequence = generated_sequence[len(flat_previous_tokens):]
 
-                if generated_sequence[0] not in self._eos_token_ids:
+                if generated_sequence[0] in self._eos_token_ids:
+                    generated_sequence[0] = END_OF_SENTENCE_TOKEN_ID
 
-                    if generated_sequence[-1] != END_OF_SENTENCE_TOKEN_ID:
-                        generated_sequence = torch.cat((generated_sequence, torch.unsqueeze(
-                            torch.tensor(END_OF_SENTENCE_TOKEN_ID, device=generated_sequence.device), dim=0)))
+                if generated_sequence[-1] != END_OF_SENTENCE_TOKEN_ID:
+                    generated_sequence = torch.cat((generated_sequence, torch.unsqueeze(
+                        torch.tensor(END_OF_SENTENCE_TOKEN_ID, device=generated_sequence.device), dim=0)))
 
-                    if len(generated_sequence) > 0:
-                        #logger.info(generated_sequence)
-                        generated_text = self._tokenizer._tokenizer.decode(generated_sequence.tolist(),
-                                                                           clean_up_tokenization_spaces=True,
-                                                                           skip_special_tokens=True)
+                if len(generated_sequence) > 0:
+                    #logger.info(generated_sequence)
+                    generated_text = self._tokenizer._tokenizer.decode(generated_sequence.tolist(),
+                                                                       clean_up_tokenization_spaces=True,
+                                                                       skip_special_tokens=True)
 
-                        if not generated_text.isspace() and sum(
-                                [s.isalnum() for s in generated_text]) >= self._min_sentence_character_length:
-                            generated_sequences.append({"text": generated_text, "tokens": generated_sequence})
+                    generated_sequences.append({"text": generated_text, "tokens": generated_sequence})
 
-                            #logger.info(generated_text, generated_sequence, log_prob)
+                    #logger.info(generated_text, generated_sequence, log_prob)
 
-                            sequences_tensor_list.append(generated_sequence)
-                            if log_prob is not None:
-                                print("Log probs size",log_prob.size())
-                                log_probs_tensor_list.append(torch.mean(log_prob[0:len(generated_sequence)]))
+                    sequences_tensor_list.append(generated_sequence)
+                    if log_prob is not None:
+                        print("Log probs size",log_prob.size())
+                        log_probs_tensor_list.append(torch.mean(log_prob[0:len(generated_sequence)]))
 
         # print(f"Generated: {generated_sequences}")
         return generated_sequences, sequences_tensor_list, log_probs_tensor_list
