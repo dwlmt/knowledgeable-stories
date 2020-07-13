af47bc0a1f57cd7deefdcc6ec4c891ec26533ddb
David Wilmot <david.wilmot@gmail.com>
2020-07-13 17:36:23 +0100

    Replace underscores when adding bools.

--- scripts/senteval_eval_saved.py
+++ scripts/senteval_eval_saved.py
@@ -20,8 +20,8 @@ import torch
 
 def add_bool_arg(parser, name, default=False):
     group = parser.add_mutually_exclusive_group(required=False)
-    group.add_argument('--' + name, dest=name, action='store_true')
-    group.add_argument('--no-' + name, dest=name, action='store_false')
+    group.add_argument('--' + name, dest=name.replace('-','_'), action='store_true')
+    group.add_argument('--no-' + name, dest=name.replace('-','_'), action='store_false')
     parser.set_defaults(**{name:default})
 
 
