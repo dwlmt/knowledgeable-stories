6b35ff9e5a42567e62c6e9c704192960b56128af
David Wilmot <david.wilmot@gmail.com>
2020-09-09 19:24:33 +0100

    Fix the passage setup.

--- scripts/create_aws_story_evaluation_tasks.py
+++ scripts/create_aws_story_evaluation_tasks.py
@@ -147,10 +147,12 @@ def create(prompts_json: str, gold_json: str, models_json: List[str], models_typ
 
             if p_key in model_val:
 
-                if "passage" in model_row_dict and len(model_row_dict["passage"]) >= min_characters:
-                    model_row_dict = {"type": model_key, "passage": model_val[p_key]["passage"][:max_characters]}
+                if "passage" in model_val[p_key]:
+                    passage = model_val[p_key]["passage"]
+                    if len(passage) >= min_characters:
+                        model_row_dict = {"type": model_key, "passage": passage[:max_characters]}
 
-                    models_rows.append(model_row_dict)
+                        models_rows.append(model_row_dict)
 
         if len(models_rows) == number_of_models:
             from random import shuffle
