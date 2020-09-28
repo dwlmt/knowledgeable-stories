a4e92e67c77e7f5b1594a7cdb974660ffb995e2a
David Wilmot <david.wilmot@gmail.com>
2020-09-28 15:48:38 +0100

    Add predictions and metrics.

--- scripts/comparative_evaluation_automatic.py
+++ scripts/comparative_evaluation_automatic.py
@@ -198,7 +198,7 @@ def create(prompts_json: str, gold_json: str, models_json: List[str], models_typ
 
             sacrebleu = datasets.load_metric('sacrebleu')
 
-            sacrebleu.add(model_2_text, model_1_text)
+            sacrebleu.add(prediction=model_2_text, reference=model_1_text)
 
 
             pairwise_comparison_list.append(model_pair_dict)
