69c8fe308e1106938e193a1a58bf9a421402219c
David Wilmot <david.wilmot@gmail.com>
2020-09-28 22:40:23 +0100

    Disable bertscore.

--- scripts/comparative_evaluation_automatic.py
+++ scripts/comparative_evaluation_automatic.py
@@ -204,7 +204,7 @@ def eval(prompts_json: str, gold_json: str, models_json: List[str], models_types
 
             meteor.add(prediction=model_2_text, reference=model_1_text)
             #bleu.add(prediction=[model_2_text], reference=[model_1_text])
-            bertscore.add(prediction=model_2_text, reference=model_1_text)
+            #bertscore.add(prediction=model_2_text, reference=model_1_text)
 
         meteor_score = meteor.compute()
         row["meteor_score"] = meteor_score
@@ -212,8 +212,8 @@ def eval(prompts_json: str, gold_json: str, models_json: List[str], models_types
         #bleu_score = bleu.compute()
         #row["bleu_score"] = bleu_score
 
-        bertscore_score = bertscore.compute(lang='en')
-        row["bert_score"] = bertscore_score
+        #bertscore_score = bertscore.compute(lang='en')
+        #row["bert_score"] = bertscore_score
 
         print(model_pair_dict)
 
