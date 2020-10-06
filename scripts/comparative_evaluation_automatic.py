dcd251a6175f991b833b798d84927705958c375f
David Wilmot <david.wilmot@ed.ac.uk>
2020-10-06 20:21:25 +0100

    Make array.

--- scripts/comparative_evaluation_automatic.py
+++ scripts/comparative_evaluation_automatic.py
@@ -205,16 +205,16 @@ def eval(prompts_json: str, gold_json: str, models_json: List[str], models_types
             model_1_text = row[f"story_{model_pair[0]}"]
             model_2_text = row[f"story_{model_pair[1]}"]
 
-            model_1_texts.append([model_1_text])
-            model_2_texts.append([model_2_text])
+            model_1_texts.append(model_1_text)
+            model_2_texts.append(model_2_text)
 
             meteor.add(prediction=model_2_text, reference=model_1_text)
 
             print(model_2_text, model_1_text)
 
 
-        bleu.add_batch(predictions=[model_2_texts], references=[model_1_texts])
-        bertscore.add_batch(predictions=model_2_texts, references=[model_1_texts])
+        bleu.add_batch(predictions=model_2_texts, references=model_1_texts)
+        bertscore.add_batch(predictions=model_2_texts, references=model_1_texts)
 
         meteor_score = meteor.compute()
         model_pair_dict["meteor_score"] = meteor_score
