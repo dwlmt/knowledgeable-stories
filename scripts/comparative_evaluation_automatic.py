57ed24a79ed61b30001b6dab40cf40c9db9e54ae
David Wilmot <david.wilmot@ed.ac.uk>
2020-10-06 20:11:04 +0100

    Fix reference.

--- scripts/comparative_evaluation_automatic.py
+++ scripts/comparative_evaluation_automatic.py
@@ -205,7 +205,7 @@ def eval(prompts_json: str, gold_json: str, models_json: List[str], models_types
             model_1_text = row[f"story_{model_pair[0]}"]
             model_2_text = row[f"story_{model_pair[1]}"]
 
-            model_1_text.append(model_1_text)
+            model_1_texts.append(model_1_text)
             model_2_texts.append(model_2_text)
 
             print(model_2_text, model_1_text)
